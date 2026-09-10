a66efc59ca922f077cb0316e1bed81bd500c5b93
Raja-06K <maharajakorga@gmail.com>
2026-09-10 09:57:22 +0700

    Add files via upload
    Praktikum pekan 2

---
+++ Pekan2/Numerik_2611532030.py
@@ -0,0 +1,13 @@
+# Buat file dengan nama Numerik_NIM.py
+# nama variabel ditambah 4 digit terakhir NIM contoh: a_5500
+# Create a variable with numeric type value.
+a_2030=100
+print("The type of variable having value", a_2030, " is ", type(a_2030))
+
+#create a variable with float value
+b_2030=10.2345
+print("The type of variable having value", b_2030, " is ", type(b_2030))
+
+#create a variable with complex value 
+c_2030=100+3j
+print("The type of variable having value", c_2030, " is ", type(c_2030))
