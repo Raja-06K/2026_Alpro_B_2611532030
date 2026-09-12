c2ebc51aedf349962dcfac6470195f36967960e4
Raja-06K <maharajakorga@gmail.com>
2026-09-12 22:54:02 +0700

    Add files via upload

---
+++ Pekan2/program2_2611532030/String_2611532030.py
@@ -0,0 +1,15 @@
+# Buat file dengan nama Sring_NIM.py
+# nama variabel ditambah 4 digit terakhir NIM contoh: nama_1234
+# ganti programnya dan data masing2
+nama_2030 = "Maharaja_Korga"
+jenis_kelamin_2030 = 'L'
+alamat_2030 = """
+    Lubuk Lintah
+    kec.Kuranji
+    kota Padang
+"""
+agama_2030 = "Islam"
+print("Nama saya adalah:", nama_2030)
+print("Jenis kelamin saya adalah:", jenis_kelamin_2030)
+print("Alamat saya adalah:", alamat_2030)
+print("agama saya adalah:", agama_2030)
